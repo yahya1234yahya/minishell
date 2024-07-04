2487a6ef372c0e3f704df9b7ad947e666ab8c7ea
yahya1234yahya <yahya.mouiguina2000@gmail.com>
2024-07-04 09:26:19 +0100

    commit

--- main.c
+++ main.c
@@ -6,8 +6,8 @@ int main()
 {
     while(1)
     {
-
         char *input = readline("\033[32mminishell\033[0m \033[34m>\033[0m ");
         printf("command works : %s\n", input);
+		free(input);
     }
 }
