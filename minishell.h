d7c5e11ee051479b6b0b9908ef22ec7eb97204d7
yahya1234yahya <yahya.mouiguina2000@gmail.com>
2024-07-04 09:02:22 +0100

    commit

---
+++ minishell.h
@@ -0,0 +1,24 @@
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <stdio.h>
+
+typedef struct s_cmd
+{
+	char	*cmd;
+	char	*args;
+	int		pipe;
+	int		redirection;  //we give each element a token
+	struct 	s_cmd *next;
+} t_cmd;
+
+// echo with -n option
+// cd with relative or absolute path
+// pwd with no options
+// export with no options
+// unset with no options
+// env with no options or arguments
+// exit with no options
+#endif
