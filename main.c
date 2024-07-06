e6ff9b298fe82a5170d2b6ef521db1a64edf293e
yahya1234yahya <yahya.mouiguina2000@gmail.com>
2024-07-06 12:56:28 +0100

    handle continue segf

--- main.c
+++ main.c
@@ -6,7 +6,7 @@ int main() {
     t_cmd cmd;
     while (1) {
         char *input = readline("\033[32mminishell\033[0m \033[34m>\033[0m ");
-        parse(&cmd, input);
+        parse(&cmd, input, 0);
         if (cmd.redirection)
             printf("there is a redariction\n");
         if (cmd.pipe)
