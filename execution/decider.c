d49135f9910dd063b1639d459d06a5a468a01ed3
yahya1234yahya <yahya.mouiguina2000@gmail.com>
2024-07-18 01:45:52 +0100

    commit

--- execution/decider.c
+++ execution/decider.c
@@ -6,7 +6,7 @@
 /*   By: mboughra <mboughra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/07 16:44:19 by mboughra          #+#    #+#             */
-/*   Updated: 2024/07/07 19:32:21 by mboughra         ###   ########.fr       */
+/*   Updated: 2024/07/18 01:19:51 by mboughra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,15 @@
 
 void decider(t_cmd *cmd, char **envp)
 {
-	if (!ft_strcmp("echo", cmd->cmd))
-		ft_echo(cmd);
-	if (!ft_strcmp("pwd", cmd->cmd))   //wa9ela fiha mochkill
-		ft_pwd(envp);
-	if (!ft_strcmp("exit", cmd->cmd))
-		exit(0);
-	if (!ft_strcmp("cd", cmd->cmd))  // hadi ghi test
-	{
-		chdir(cmd->args);
-	}
+	// if (!ft_strcmp("echo", cmd->cmd))
+	// 	ft_echo(cmd);
+	// if (!ft_strcmp("pwd", cmd->cmd))   //wa9ela fiha mochkill
+	// 	ft_pwd(envp);
+	// if (!ft_strcmp("exit", cmd->cmd))
+	// 	exit(0);
+	// if (!ft_strcmp("cd", cmd->cmd))  // hadi ghi test
+	// {
+	// 	chdir(cmd->args);
+	// }
+	notbuilt(cmd);
 }
