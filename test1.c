1c2603f9c73456761d3c17c051b9d6a8d0d85261
Mehdi Boughrara <mboughra@e3r1p5.1337.ma>
2024-07-28 05:55:36 +0100

    dsd

---
+++ test1.c
@@ -0,0 +1,10 @@
+// #include "minishell.h"
+
+
+// int main(int argc, char **argv, char **envp)
+// {
+// 	int fd = open("main.c", O_RDONLY);
+// 	printf("%d\n", fd);
+// 	printf("%s\n", get_next_line(fd));
+// 	return 0;
+// }
