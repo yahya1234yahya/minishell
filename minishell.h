3674c1afa4b0a45c864d96397dfc74523ca57b00
yahya1234yahya <yahya.mouiguina2000@gmail.com>
2024-09-21 12:47:30 +0100

    push

--- minishell.h
+++ minishell.h
@@ -120,6 +120,9 @@ char	*ft_strchr(const char *s, int c); //TODO
 char	*ft_itoa(int n);
 int	ft_isalpha(int c);
 int	ft_isdigit(int c);
+int	ft_strncmp(const char	*s1, const char	*s2, size_t	n);
+int	ft_strcmp2(const char *s1, const char *s2);
+int	ft_tolower(int c);
 
 
 // echo with -n option
